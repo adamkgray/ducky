7481c22eb31c0e55e6f8a90874f5c45e2c12cd09
Adam K Gray <adamkgray@pm.me>
2022-02-19 16:39:55 +0000

    add comments

--- ducky.m
+++ ducky.m
@@ -25,18 +25,36 @@ classdef ducky < handle
             obj.Activation = activation;
 
             % initialise weights
+            % since weights are in between layers,
+            % there will always be L - 1 weight matrixes
             weights = cell(1, length(layers) - 1);
 
             for i = 1:(length(layers) - 1)
+
+                % the weights connecting two layers are stored
+                % as an NxM matrix where 'N' is the number of nodes
+                % in the parent layer and 'M' is the number of nodes
+                % in the child layer
+
+                % add one node to each weight matrix
+                % as a bias node
                 n = layers(i) + 1;
                 m = layers(i + 1) + 1;
 
-                % the last weight layer is different
+                % the last weight matrix for is the output
+                % it doesn't need the bias node
                 if i == (length(layers) - 1)
                     m = layers(end);
                 end
 
+                % dividing the randomised weight be the
+                % square root of the original number
+                % of nodes in the layer helps avoid the
+                % 'vanishing gradient' problem
+                % see this answer on stack exchange:
+                % https://stats.stackexchange.com/questions/326710/why-is-weight-initialized-as-1-sqrt-of-hidden-nodes-in-neural-networks
                 w = normrnd(0, 1, [n m]) / sqrt(layers(i));
+
                 weights{i} = w;
             end
 
@@ -81,24 +99,33 @@ classdef ducky < handle
         %%%%%%%%%%%%%%%%%%%%%
         % sum squared error %
         %%%%%%%%%%%%%%%%%%%%%
-        function errors = sse(obj, x, t)
+        function error = sse(obj, x, t)
             p = predict(obj, x);
-            errors = sum((t - p).^2) / 2;
+            error = sum((t - p).^2) / 2;
         end
 
         %%%%%%%%%%%%%%%%%
         % train network %
         %%%%%%%%%%%%%%%%%
         function errors = train(obj, x, t, epochs)
-            % add bias feature
+            % since our weights include an extra node
+            % for the bias, we need to add an extra
+            % feature to our data
+            % this feature is just a '1' which will
+            % then be continually scaled for each layer
             xWithBias = [x ones(size(x, 1), 1)];
 
-            % keep track of SSE
+            % store the error rate for each epoch
+            % in a vector
             errors = zeros(1, epochs);
 
+            % iterate over epochs
             for e = 1:epochs
 
                 % fit training data
+                % this means for each sample we
+                % conduct a forward pass and
+                % then do backpropagation
                 for i = 1:size(xWithBias, 1)
                     fit(obj, xWithBias(i, :), t(i, :))
                 end
@@ -128,9 +155,14 @@ classdef ducky < handle
 
             % iterate over layers (technically, the weights)
             for i = 1:(length(obj.Weights))
+                % the weighted sums for the next layer
+                % of neurons is just the matrix multiplication
+                % of the activations and the weights
                 net = activations{i} * obj.Weights{i};
                 weightedSums{i + 1} = net;
 
+                % the activation function is then applied
+                % to each value (i.e. node) in the resulting matrix
                 net = activation(obj, net);
                 activations{i + 1} = net;
             end
@@ -139,6 +171,7 @@ classdef ducky < handle
             % backpropagation %
             %%%%%%%%%%%%%%%%%%%
 
+            % compute the cost
             cost = activations{end} - t;
 
             % deltas go from last layer to first layer
@@ -160,7 +193,10 @@ classdef ducky < handle
             % recently computed delta, i.e the backwards
             % propagation
             for i = (length(obj.Weights)):-1:2
-                delta = (deltas{j} * obj.Weights{i}') .* activationDerivative(obj, weightedSums{i});
+                delta = ...
+                    (deltas{j} * obj.Weights{i}') ...
+                    .* activationDerivative(obj, weightedSums{i} ...
+                );
                 j = j + 1;
                 deltas{j} = delta;
             end
@@ -173,7 +209,11 @@ classdef ducky < handle
 
             % update the weights
             for i = 1:length(obj.Weights)
-                obj.Weights{i} = obj.Weights{i} + (-obj.LearningRate * (activations{i}' * deltas{i}));
+                obj.Weights{i} = ...
+                    obj.Weights{i} + ( ...
+                    -obj.LearningRate * ( ...
+                    activations{i}' * deltas{i} ...
+                ));
             end
 
         end
